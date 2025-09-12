6119aeeeed272fecaa49bbdabf41181bce74f6da
Ernest Talagtag <ernestneil09@gmail.com>
2025-09-13 03:59:19 +0800

    Fixed The Issue: when adding to the cart, it was creating a new object in the array

--- data/cart.js
+++ data/cart.js
@@ -6,17 +6,14 @@ export let cart = [{
     productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
     quantity: 1   
 }, 
-{
-    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 2
-},];
+];
 
 export function addToCart(productId){
   const productQuantity = document.querySelector(`.js-quantity-selector-${productId}`); 
   let matchingItem;
   
   cart.forEach((cartItem) => {            //checking if the dataset.productName === cart.name
-    if(cartItem.id === productId){
+    if(cartItem.productId === productId){
       matchingItem = cartItem;
     }
   });
